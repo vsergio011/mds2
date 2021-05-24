bc6e7e02dc47540c1fb7893870fe123b313b0c05
sergio vera <Sergio Vera>
2021-05-24 17:49:05 +0200

    habilitacion de botones del editar

--- mdsdos/src/main/java/tiendaVirtual/interfaz/Modificar_Datos.java
+++ mdsdos/src/main/java/tiendaVirtual/interfaz/Modificar_Datos.java
@@ -1,5 +1,10 @@
 package tiendaVirtual.interfaz;
 
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
 import vistas.VistaModificargeneral;
 
 public class Modificar_Datos extends VistaModificargeneral {
@@ -8,6 +13,7 @@ public class Modificar_Datos extends VistaModificargeneral {
 	private Object _comprasB;
 	private Object _desconectarB;
 	public Perfil _perfil;
+	public VerticalLayout containerInfo;
 	
 	public Modificar_Datos() {
 	}
@@ -21,8 +27,78 @@ public class Modificar_Datos extends VistaModificargeneral {
 		this.getImg().setSrc(ciber.getFoto());
 		Modificar_general _Modificar_general = new Modificar_general();
 		_Modificar_general.fillData(ciber);
-		this.getContainerLayout().removeAll();
-		this.getContainerLayout().add(_Modificar_general);
+		containerInfo = this.getContainerLayout();
+		containerInfo.removeAll();
+		containerInfo.add(_Modificar_general);
+		
+		
+		this.getCambiarPasswordBtn().addClickListener(new ComponentEventListener<ClickEvent<Button>>() {
+
+			@Override
+			public void onComponentEvent(ClickEvent<Button> event) {
+				containerInfo.removeAll();
+				Cambiar_Contrasena _Cambiar_Contrasena = new Cambiar_Contrasena();
+				containerInfo.add(_Cambiar_Contrasena);
+				
+			}
+		});
+		
+		
+		this.getComprasBtn().addClickListener(new ComponentEventListener<ClickEvent<Button>>() {
+
+			@Override
+			public void onComponentEvent(ClickEvent<Button> event) {
+				containerInfo.removeAll();
+				/*Cambiar_Contrasena _Cambiar_Contrasena = new Cambiar_Contrasena();
+				containerInfo.add(_Cambiar_Contrasena);*/
+				
+			}
+		});
+		
+		this.getContactoBTn().addClickListener(new ComponentEventListener<ClickEvent<Button>>() {
+
+			@Override
+			public void onComponentEvent(ClickEvent<Button> event) {
+				containerInfo.removeAll();
+				/*Cambiar_Contrasena _Cambiar_Contrasena = new Cambiar_Contrasena();
+				containerInfo.add(_Cambiar_Contrasena);*/
+				
+			}
+		});
+		
+		this.getBorrarCuentaBtn().addClickListener(new ComponentEventListener<ClickEvent<Button>>() {
+
+			@Override
+			public void onComponentEvent(ClickEvent<Button> event) {
+				containerInfo.removeAll();
+				/*Cambiar_Contrasena _Cambiar_Contrasena = new Cambiar_Contrasena();
+				containerInfo.add(_Cambiar_Contrasena);*/
+				
+			}
+		});
+		
+		this.getDesconectarBtn().addClickListener(new ComponentEventListener<ClickEvent<Button>>() {
+
+			@Override
+			public void onComponentEvent(ClickEvent<Button> event) {
+				containerInfo.removeAll();
+				/*Cambiar_Contrasena _Cambiar_Contrasena = new Cambiar_Contrasena();
+				containerInfo.add(_Cambiar_Contrasena);*/
+				
+			}
+		});
+		
+		this.getAceptarCambiosBtn().addClickListener(new ComponentEventListener<ClickEvent<Button>>() {
+
+			@Override
+			public void onComponentEvent(ClickEvent<Button> event) {
+				containerInfo.removeAll();
+				/*Cambiar_Contrasena _Cambiar_Contrasena = new Cambiar_Contrasena();
+				containerInfo.add(_Cambiar_Contrasena);*/
+				
+			}
+		});
+		
 	}
 	
 	public void showLess() {
