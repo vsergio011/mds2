96ad5ad9585b64c5e4c5f22a05cc4cb254fa16cf
Antonio Miguel Fernandez Jimenez <afj014@inlumine.ual.es>
2021-05-27 19:09:43 +0200

    Evita error al cargar productos

--- mdsdos/src/main/java/tiendaVirtual/interfaz/Detalle_Producto_Admin.java
+++ mdsdos/src/main/java/tiendaVirtual/interfaz/Detalle_Producto_Admin.java
@@ -23,12 +23,10 @@ public class Detalle_Producto_Admin extends Detalle_Producto {
 		appventawebbd.Oferta oferta = admin.estaEnOferta(producto.getId());
 		this.getDeleteBtn().setVisible(oferta != null);
 		
+		// TODO: No se deberia mostrar el boton.
+		 _quitarOferta = new Quitar_Oferta();
 		if (oferta != null) {
 			_quitarOferta = new Quitar_Oferta(oferta);
-		} else {
-			// TODO: No se deberia mostrar el boton.
-			// _quitarOferta = new Quitar_Oferta();
 		}
 	}
-	
 }
