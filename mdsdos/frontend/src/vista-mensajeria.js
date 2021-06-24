5bfa86d265c221a8e6c44264a7b0e72f80154264
sergio vera <Sergio Vera>
2021-06-24 13:10:04 +0200

    vista mensajeria cambio visual

--- mdsdos/frontend/src/vista-mensajeria.js
+++ mdsdos/frontend/src/vista-mensajeria.js
@@ -1,8 +1,8 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
-import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
-import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
-import '@vaadin/vaadin-button/src/vaadin-button.js';
 import '@polymer/iron-icon/iron-icon.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import '@vaadin/vaadin-button/src/vaadin-button.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
 
 class VistaMensajeria extends PolymerElement {
 
@@ -19,31 +19,31 @@ class VistaMensajeria extends PolymerElement {
  <label id="nombreBtn"></label>
  <vaadin-vertical-layout theme="spacing" style="width: 80%; margin-top: 2%;">
   <label style="border:solid; align-self: center;" id="tituloLbl"></label>
-  <vaadin-button theme="icon" aria-label="Add new" id="reloadBtn">
+  <vaadin-button theme="icon" aria-label="Add new" id="reloadBtn" style="background-color: #006400; border-radius: 40px 40px 40px 40px;">
    <iron-icon icon="lumo:reload"></iron-icon>
   </vaadin-button>
  </vaadin-vertical-layout>
- <vaadin-vertical-layout id="vaadinVerticalLayout" style="width: 100%;"></vaadin-vertical-layout>
- <vaadin-horizontal-layout theme="spacing" style="width: 80%; margin-top: 2%;">
-  <vaadin-vertical-layout theme="spacing" style="width: 100%; align-items: flex-end;">
-   <vaadin-button id="recibidosBtn">
-     Recibidos 
-   </vaadin-button>
+ <vaadin-vertical-layout id="vaadinVerticalLayout" style="width: 100%;background-color:#778899"></vaadin-vertical-layout>
+ <vaadin-horizontal-layout style="align-self: stretch; justify-content: space-between;background-color :#374453">
+  <vaadin-horizontal-layout theme="spacing" style="justify-content: flex-start;">
    <vaadin-button id="enviadosBtn">
      Enviados 
+   </vaadin-button>
+   <vaadin-button id="recibidosBtn">
+     Recibidos 
    </vaadin-button>
    <vaadin-button id="verMensajeBtn">
      Ver mensaje 
    </vaadin-button>
-  </vaadin-vertical-layout>
-  <vaadin-vertical-layout theme="spacing" style="width: 100%;">
-   <vaadin-button id="nuevoMensajeBtn">
-     Nuevo mensaje 
-   </vaadin-button>
+  </vaadin-horizontal-layout>
+  <vaadin-horizontal-layout theme="spacing">
    <vaadin-button id="responderBtn">
      Responder 
    </vaadin-button>
-  </vaadin-vertical-layout>
+   <vaadin-button id="nuevoMensajeBtn">
+     Nuevo mensaje 
+   </vaadin-button>
+  </vaadin-horizontal-layout>
  </vaadin-horizontal-layout>
 </vaadin-vertical-layout>
 `;
